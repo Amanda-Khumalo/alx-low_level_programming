51f91ea585f7057d9fed72e2749c2c02bc9e0fb8
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-16 14:40:39 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (approved)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcomeis a piece of art,\n");
+	return (0);
+}
