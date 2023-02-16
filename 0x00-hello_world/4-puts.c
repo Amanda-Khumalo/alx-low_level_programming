c199b952b8899c189325b16b5164c45677f540fa
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-16 14:28:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Return: Always 0 (approved)
+ * main - Enter point
+ * Return: 0 (approved)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
