7ee7597d462320403e22fa879b8351c27dda7832
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-17 15:07:59 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+i#include <stdio.h>
+#include <string.h>
+/**
+ * main - main function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char chLower = 'a';
+	char chUpper = 'A';
+
+	for (chLower = 'a'; chLower <= 'z'; chLower++)
+	{
+		putchar(chLower);
+	}
+	for (chUpper = 'A'; chUpper <= 'Z'; chUpper++)
+	{
+		putchar(chUpper);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
