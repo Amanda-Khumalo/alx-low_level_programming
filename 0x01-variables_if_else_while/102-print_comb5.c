87e35c9160e35dfd2c88073591e259148a1f1365
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-18 12:44:09 -0800

    print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,18 +6,41 @@
  */
 
 int main(void)
+{
+	int c = 0;
+	int d;
+	int e;
 
-	int c = 0:
-	int f_d;
-	int l_d;
-
-	int c2
-	int f_d2;
-	int l_d2;
+	int c1;
+	int d1;
+	int e1;
 
 	while (c <= 98)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 + 0;
-
+		d = (c / 10 + '0');
+		e = (c % 10 + '0');
+		c1 = 0;
+		while (c1 <= 99)
+		{
+			d1 = (c / 10 + '0');
+			e1 = (c % 10 + '0');
+			if (c < c1)
+			{
+				putchar(d);
+				putchar(e);
+				putchar(' ');
+				putchar(d1);
+				putchar(e1);
+				if (c != 98)
+				{
+					putchar(' ');
+					putchar(' ');
+				}
+			}
+			c1++;
+		}
+		c1++;
+	}
+	putchar('\n');
+	return (0);
+}
