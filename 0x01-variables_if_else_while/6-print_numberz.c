c4919ca28d2f9bce9e4bd89bfbe12ac39f2b14f4
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-17 17:59:45 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,10 @@
 
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-
-	printf("\n");
-
-	return (0);
+	for (i = 0 ;  i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return(0);
 }
