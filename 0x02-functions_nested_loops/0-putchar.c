50ea351defdfc09a14311ab9fa8a442326d59004
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-21 11:12:45 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints _putchar and a new linw
+ * 
  * Return: Always 0
  */
 
@@ -16,7 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 
 	return (0);
 }
