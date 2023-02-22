c2377e42fa18b862bf5998c5b822a8c76da88f5b
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-22 11:25:28 -0800

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,12 +20,16 @@ void jack_bauer(void)
 			{
 				for (m = 0; m <= 5; m++)
 				{
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar(58);
-					_putchar(m + '0');
-					_putchar(n + '0');
-					_putchar('\n');
+					for (n = 0; n <= 9; n++)
+					{
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar(58);
+						_putchar(m + '0');
+						_putchar(n + '0');
+						_putchar('\n');
+
+					}
 				}
 			}
 		}
