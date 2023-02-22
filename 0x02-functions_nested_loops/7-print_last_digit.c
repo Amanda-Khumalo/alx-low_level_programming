95e90d36d82502e54952586265ee56cb0ce307ae
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-22 08:36:21 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @r: number to be checked
+ *
+ * Return: the last digit number
+ */
+
+int print_last_digit(int r)
+{
+	int ld = n % 10
+
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+
+	return (0);
+}
