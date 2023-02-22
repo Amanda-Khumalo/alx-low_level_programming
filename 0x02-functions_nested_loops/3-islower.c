cfdfc15ea86a51c09c9880beb707b60e26058738
Amanda-Khumalo <amandapkhumalo@icloud.com>
2023-02-21 18:46:08 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 include "main.h"
 
 /**
- * _islower - checks is a character is lower
+ * _islower -  checks if a character is lowercase
  * @c: the character
  * Return: 1 if letter is lower, 0 if not
  */
